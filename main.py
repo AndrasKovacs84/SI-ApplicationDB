2a465a87908d14b92bda66c31874d146b7543e53
AndrasKovacs84 <ka841001@gmail.com>
2017-05-09 17:27:18 +0200

    required functions added and working

--- main.py
+++ main.py
@@ -1,8 +1,126 @@
+import os
 import queries
+import psycopg2
+
+
+def clear_screen():
+    if os.name == 'posix':
+        os.system('clear')
+    if os.name == 'nt':
+        os.system('cls')
+
+
+def print_data(data):
+    '''Pretty prints data given to it. Presumes that the data parameter
+    is a nested list: data[0] = list of column names, data[1] = list of tuples
+    each tuple represents a row to be printed'''
+    clear_screen()
+    column_widths = []
+    total_width = 0
+    # Determine how wide the printed table needs to be
+    # Builds a list, where each element is the width of a column to be printed
+    for column_name in data[0]:
+        column_widths.append(len(column_name))
+
+    for row in data[1]:
+        for index, column in enumerate(row):
+            if column_widths[index] < len(str(column)):
+                column_widths[index] = len(str(column))
+
+    # total_width only used to print a separating line under the column names
+    for number in column_widths:
+        total_width += (number + 2)
+
+    # Print column names
+    for index, column_name in enumerate(data[0]):
+        print(str(data[0][index]).rjust(column_widths[index]), end="  ")
+    # Print separating line
+    print("\n" + "=" * (total_width))
+    # Print the rows of data
+    for row in data[1]:
+        for index, column in enumerate(row):
+            print(str(column).rjust(column_widths[index]), end="  ")
+        print("")
+
+
+def menu():
+    '''Function to handle the menu and navigate the various queries'''
+    menu_options = ['List mentor names',
+                    'List mentor nicknames',
+                    'Find applicant "Carol"',
+                    'Find applicant by email',
+                    'Insert new applicant Markus',
+                    'Update applicant tel nr, Jemima',
+                    'Delete applicants by email',
+                    'Display mentors table',
+                    'Display applicants table']
+    for index, menu_item in enumerate(menu_options):
+        print(str(index + 1), "-", str(menu_item))
+    print("0 - Exit")
+
+    valid_option = False
+    choice = 0
+    while not valid_option:
+        try:
+            choice = int(input("Please enter chosen menu option: "))
+        except ValueError:
+            print('Chosen option must be a number between 0 and 9')
+            continue
+        if choice >= 0 and choice <= 9:
+            valid_option = True
+        else:
+            print('Chosen option must be a number between 0 and 9')
+
+    if choice == 0:
+        print('\n')
+        print('exiting...')
+        quit()
+    elif choice == 1:
+        print_data(queries.mentor_name())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 2:
+        print_data(queries.mentor_nicks())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 3:
+        print_data(queries.applicant_carol())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 4:
+        print_data(queries.find_applicant_by_email())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 5:
+        try:
+            print_data(queries.new_applicant_insert_select())
+        except psycopg2.IntegrityError as err:
+            print(err)
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 6:
+        print_data(queries.jemima_tel_nr_update())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 7:
+        print_data(queries.delete_by_email())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 8:
+        print_data(queries.show_mentors())
+        print('\n')
+        input('Press enter to return to menu')
+    elif choice == 9:
+        print_data(queries.show_applicants())
+        print('\n')
+        input('Press enter to return to menu')
 
 
 def main():
-    pass
+    while True:
+        clear_screen()
+        menu()
+
 
 if __name__ == '__main__':
     main()
--- queries.py
+++ queries.py
@@ -1,68 +1,160 @@
 import psycopg2
 
-connect_str = "dbname='andras' user='andras' host='localhost' password='andras'"
+# connect_str = "dbname='andras' user='andras' host='localhost' password='andras'"
+connect_str = "dbname='apha' user='apha' host='localhost' password='AndrasKovacs84'"
 conn = psycopg2.connect(connect_str)
 conn.autocommit = True
 cursor = conn.cursor()
 
 
-'''
-cursor.execute("""SELECT * FROM applicants""")
-rows = cursor.fetchall()
-print(rows)'''
-
-
-def qry_mentor_name():
+def mentor_name():
     '''Write a query that returns the 2 name columns of the mentors table.
     columns: first_name, last_name'''
-    pass
+    data = []
+    cursor.execute("""SELECT first_name as "First Name", last_name as "Last Name"
+                   FROM mentors""")
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
 
-def qry_mentor_nicks():
+def mentor_nicks():
     '''Write a query that returns the nick_name-s of all mentors working at Miskolc.
     column: nick_name'''
-    pass
+    data = []
+    cursor.execute("""
+                   SELECT nick_name as "Nickname"
+                   FROM mentors
+                   WHERE city='Miskolc';
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
 
-def qry_applicant_carol():
+def applicant_carol():
     '''We had interview with an applicant, some Carol. We don't remember her name, but she left her hat at the school.
     We want to call her to give her back her hat. To look professional, we also need her full name when she answers
     the phone (for her full_name, you want to include a concatenation into your query, to get her full_name,
     like: "Carol Something" instead of having her name in 2 different columns in the result.
     This columns should be called: full_name). columns: full_name, phone_number'''
-    pass
+    data = []
+    cursor.execute("""
+                   SELECT CONCAT(first_name, ' ', last_name) as "Full name", phone_number as "Phone number"
+                   FROM applicants
+                   WHERE first_name='Carol';
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
 
-def qry_find_applicant_by_email():
+def find_applicant_by_email():
     '''We called Carol, and she said it's not her hat. It belongs to another girl, who went to the famous
     Adipiscingenimmi University. You should write a query to get the same informations like with Carol,
     but for this other girl. The only thing we know about her is her school e-mail address ending:
     '@adipiscingenimmi.edu'. columns: full_name, phone_number'''
-    pass
+    data = []
+    cursor.execute("""
+                   SELECT CONCAT(first_name, ' ', last_name) AS "Full Name", phone_number AS "Phone Number"
+                   FROM applicants
+                   WHERE email LIKE '%@adipiscingenimmi.edu';
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
 
-def qry_new_applicant_insert_select():
+def new_applicant_insert_select():
     '''After we returned the hat, a new applicant appeared at the school, and he wants to get into the application process.
     His name is Markus Schaffarzyk, has a number: 003620/725-2666 and e-mail address: djnovus@groovecoverage.com
     Our generator gave him the following application code: 54823
     After INSERTing the data, write a SELECT query, that returns with all the columns of this applicant!
     (use the unique application code for your condition!)'''
-    pass
+    data = []
+    cursor.execute("""
+                   INSERT INTO applicants
+                   (first_name, last_name, phone_number, email, application_code)
+                   VALUES
+                   ('Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com', 54823 )
+                   """)
+    cursor.execute("""
+                   SELECT *
+                   FROM applicants
+                   WHERE application_code=54823
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
 
-def qry_jemima_tel_nr_update():
+def jemima_tel_nr_update():
     '''Jemima Foreman, an applicant called us, that her phone number changed to: 003670/223-7459
     Write an UPDATE query, that changes this data in the database for this applicant.
     Also, write a SELECT query, that checks the phone_number column of this applicant.
     Use both of her name parts in the conditions!'''
-    pass
+    data = []
+    cursor.execute("""
+                   UPDATE applicants
+                   SET phone_number='003670/223-7459'
+                   WHERE first_name='Jemima' AND last_name='Foreman'
+                   """)
+    cursor.execute("""
+                   SELECT *
+                   FROM applicants
+                   WHERE first_name='Jemima' AND last_name='Foreman'
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
 
-def qry_delete_by_email():
+def delete_by_email():
     '''Arsenio, an applicant called us, that he and his friend applied to Codecool.
     They both want to cancel the process, because they got an investor for the site they run: mauriseu.net
     Write DELETE query to remove all the applicants, who applied with emails for this domain
     (e-mail address has this domain after the @ sign).'''
-    pass
+    cursor.execute("""
+                   DELETE FROM applicants
+                   WHERE email LIKE '%@mauriseu.net'""")
+    data = [['Taken action'], [['Applicants deleted']]]
+    return data
 
+
+def show_mentors():
+    data = []
+    cursor.execute("""
+                   SELECT *
+                   FROM mentors
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
+
+
+def show_applicants():
+    data = []
+    cursor.execute("""
+                   SELECT *
+                   FROM applicants
+                   """)
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
 
